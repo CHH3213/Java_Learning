737b22f87946d00c2a922fd2669d8e8e87e96455
CHH3213 <1213472983@qq.com>
2022-01-04 23:12:40 +0800

    添加idea

---
+++ IDEA_workspace/Multi_Thread/Thread_creation/src/pers/chh3213/thread/ThreadDemo.java
@@ -0,0 +1,56 @@
+package pers.chh3213.thread;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : chh3213
+ * @version : 1.0
+ * @Project : Multi_Thread
+ * @Package : pers.chh3213.thread
+ * @FileName : ThreadDemo.java
+ * @createTime : 2022/1/4 下午9:01
+ * @Email :
+ * @Description :
+ */
+class RunnableDemo implements Runnable{
+    private Thread t;
+    private String threadName;
+
+    RunnableDemo( String name) {
+        threadName = name;
+        System.out.println("Creating " +  threadName );
+    }
+    @Override
+    public void run() {
+        System.out.println("Running " +  threadName );
+        try {
+            for(int i = 4; i > 0; i--) {
+                System.out.println("Thread: " + threadName + ", " + i);
+                // 让线程睡眠一会
+                Thread.sleep(50);
+            }
+        }catch (InterruptedException e) {
+            System.out.println("Thread " +  threadName + " interrupted.");
+        }
+        System.out.println("Thread " +  threadName + " exiting.");
+    }
+
+    public void start () {
+        System.out.println("Starting " +  threadName );
+        if (t == null) {
+            t = new Thread (this, threadName);
+            t.start ();
+        }
+    }
+}
+
+public class ThreadDemo {
+
+    public static void main(String args[]) {
+        RunnableDemo R1 = new RunnableDemo( "Thread-1");
+        R1.start();
+
+        RunnableDemo R2 = new RunnableDemo( "Thread-2");
+        R2.start();
+    }
+}
