b17fbb476bcfbb30c4f9b6914578ab51142d41b6
CHH3213 <1213472983@qq.com>
2022-01-05 08:42:59 +0800

    windows测试idea

---
+++ IDEA_workspace/src/pers/llo.java
@@ -0,0 +1,11 @@
+package pers;/**
+ * Created with IntelliJ IDEA.
+ * @Project      : TestIDEA
+ * @Package      : pers
+ * @createTime   : 2022/1/4 9:22
+ * @version      : 1.0
+ * @author       : chh3213
+ * @Email        : 
+ * @Description  :
+ */public interface llo {
+}
---
+++ IDEA_workspace/src/pers/test.java
@@ -0,0 +1,2 @@
+package pers;public interface test {
+}
