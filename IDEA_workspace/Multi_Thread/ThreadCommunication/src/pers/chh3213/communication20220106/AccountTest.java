989e3e6de126abe0fb1261169d54762c92630c83
CHH3213 <1213472983@qq.com>
2022-01-06 22:49:22 +0800

    线程

--- IDEA_workspace/Multi_Thread/ThreadCommunication/src/pers/chh3213/communication20220106/AccountTest.java
+++ IDEA_workspace/Multi_Thread/ThreadCommunication/src/pers/chh3213/communication20220106/AccountTest.java
@@ -7,12 +7,12 @@ package pers.chh3213.communication20220106;
  * @version : 1.0
  * @Project : Multi_Thread
  * @Package : pers.chh3213.communication20220106
- * @FileName : Account.java
+ * @FileName : AccountTest.java
  * @createTime : 2022/1/6 上午9:46
  * @Email :
  * @Description :模拟银行取钱的问题
  */
-public class Account {
+class Account {
     private String name;
     private double balance;
 
@@ -41,18 +41,99 @@ public class Account {
     public String toString(){
         return "Account[name="+name+",balance="+balance+"]";
     }
-    
-
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 :
+                name.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(balance);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Account other = (Account) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (Double.doubleToLongBits(balance) !=
+                Double.doubleToLongBits(other.balance))
+            return false;
+        return true;
+    }
 }
 
-class XiaoMing extends Account implements Runnable{
-    XiaoMing(String name,double balance){
-        super(name,balance);
+
+class XiaoMing implements Runnable{
+    private Account account;
+    private double withdraw;
+    XiaoMing(Account account,double amt){
+        this.account =account;
+        this.withdraw = amt;
     }
     @Override
-    public synchronized void run(){
-        if(super.getBalance()>0){
-
+    public  void run(){
+        synchronized (account) {//锁别加错
+            if (account.getBalance() > withdraw) {
+                System.out.println(Thread.currentThread().getName() + "取款成功，金额为：+" + withdraw);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                account.setBalance(account.getBalance() - withdraw);
+            } else {
+                System.out.println("取款失败,当前余额为：" + account.getBalance());
+            }
         }
+    }
+}
+
+class WithDrawThread extends Thread {
+    Account account;
+    // 要取款的额度
+    double withDraw;
+    public WithDrawThread(String name, Account account, double amt) {
+        super(name);
+        this.account = account;
+        this.withDraw = amt;
+    }
+    public void run() {
+        synchronized (account) {
+            if (account.getBalance() > withDraw) {
+                System.out.println(Thread.currentThread().getName() + ":取款成功，取现的金额为：" + withDraw);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                account.setBalance(account.getBalance() - withDraw);
+            } else {
+                System.out.println("取现额度超过账户余额，取款失败");
+            }
+            System.out.println("现在账户的余额为：" + account.getBalance());
+        }
+    }
+}
+
+public  class AccountTest {
+    public static void main(String[] args) {
+        Account account = new Account("1111",10000);
+        XiaoMing xiaoMing = new XiaoMing(account,8000);
+        XiaoMing xiaoMingWife = new XiaoMing(account,8000);
+        Thread t1 = new Thread(xiaoMing);
+        Thread t2 = new Thread(xiaoMingWife);
+        t1.setName("小明");
+        t2.setName("小明's wife");
+        t1.start();
+        t2.start();
     }
 }
--- IDEA_workspace/Multi_Thread/ThreadCommunication/src/pers/chh3213/communication20220106/ProductTest.java
+++ IDEA_workspace/Multi_Thread/ThreadCommunication/src/pers/chh3213/communication20220106/ProductTest.java
@@ -1,12 +1,106 @@
-package pers.chh3213.communication20220106;/**
+package pers.chh3213.communication20220106;
+
+/**
  * Created with IntelliJ IDEA.
- * @Project      : Multi_Thread
- * @Package      : pers.chh3213.communication20220106
- * @FileName    : ProductTest.java
- * @createTime   : 2022/1/6 上午9:15
- * @version      : 1.0
- * @author       : chh3213
- * @Email        : 
- * @Description  :
- */public class ProductTest {
+ *
+ * @author : chh3213
+ * @version : 1.0
+ * @Project : Multi_Thread
+ * @Package : pers.chh3213.communication20220106
+ * @FileName : ProductTest.java
+ * @createTime : 2022/1/6 上午9:15
+ * @Email :
+ * @Description :经典例题：生产者/消费者问题
+ */
+public class ProductTest {
+    public static void main(String[] args) {
+        Clerk clerk = new Clerk(0);
+        Customer customer1 =new Customer(clerk);
+        Customer customer2 =new Customer(clerk);
+        Productor productor = new Productor(clerk);
+        Thread thread1 = new Thread(customer1);
+        Thread thread3 = new Thread(customer2);
+        Thread thread2 = new Thread(productor);
+        thread2.setName("生产者");
+        thread1.setName("消费者1");
+        thread3.setName("消费者2");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
+
+class Clerk{
+    private int product = 0;
+    Clerk(int product){
+        this.product=product;
+    }
+    public synchronized void toProduct(){
+        if(product>=20){
+            try{
+                wait();
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        else{
+            product++;
+            System.out.println(Thread.currentThread().getName()+"生产第"+product+"个产品");
+            notifyAll();
+        }
+    }
+    public synchronized void getProduct(){
+        if(product<=0){
+            try {
+                wait();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        else{
+            product--;
+            System.out.println(Thread.currentThread().getName()+"拿走了第"+product+"个产品");
+            notifyAll();
+        }
+    }
+}
+
+
+class  Productor implements Runnable{
+    private Clerk clerk;
+    Productor(Clerk clerk){
+        this.clerk = clerk;
+    }
+    @Override
+    public void run(){
+        System.out.println(Thread.currentThread().getName()+"开始生产产品");
+        while (true) {
+            try {
+                Thread.sleep(10);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            clerk.toProduct();
+        }
+
+    }
+}class  Customer implements Runnable{
+    private Clerk clerk;
+    Customer(Clerk clerk){
+        this.clerk = clerk;
+    }
+    @Override
+    public void run(){
+        System.out.println(Thread.currentThread().getName()+"开始取走产品");
+        while (true) {
+            try {
+                Thread.sleep(10);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            clerk.getProduct();
+        }
+    }
+
 }
