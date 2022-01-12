ff30c64e99823346efbb6f1c1021ce7da8c0d064
CHH3213 <1213472983@qq.com>
2022-01-12 23:36:41 +0800

    map完结

--- IDEA_workspace/Collection_Map/Map_Module/src/pers/chh3213/map/PropertiesTest.java
+++ IDEA_workspace/Collection_Map/Map_Module/src/pers/chh3213/map/PropertiesTest.java
@@ -1,12 +1,45 @@
-package pers.chh3213.map;/**
+package pers.chh3213.map;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+/**
  * Created with IntelliJ IDEA.
- * @Project      : Collection_Map
- * @Package      : pers.chh3213.map
- * @FileName    : PropertiesTest.java
- * @createTime   : 2022/1/12 下午11:00
- * @version      : 1.0
- * @author       : chh3213
- * @Email        : 
- * @Description  :
- */public class PropertiesTest {
+ *
+ * @author : chh3213
+ * @version : 1.0
+ * @Project : Collection_Map
+ * @Package : pers.chh3213.map
+ * @FileName : PropertiesTest.java
+ * @createTime : 2022/1/12 下午11:00
+ * @Email :
+ * @Description :
+ */
+
+public class PropertiesTest {
+    //Properties:常用来处理配置文件。key和value都是String类型
+    public static void main(String[] args){
+        //快捷键：ALT+Shift+Z
+        FileInputStream fis = null;
+        try {
+            Properties pros = new Properties();
+            fis = new FileInputStream("jdbc.properties");
+            pros.load(fis); //加载流对应文件
+
+            String name = pros.getProperty("name");
+            String password = pros.getProperty("password");
+
+            System.out.println("name = " + name + ",password = " + password);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
--- IDEA_workspace/Collection_Map/Map_Module/src/pers/chh3213/map/TreeMapTest.java
+++ IDEA_workspace/Collection_Map/Map_Module/src/pers/chh3213/map/TreeMapTest.java
@@ -1,4 +1,4 @@
-package pers.chh3213.Set;
+package pers.chh3213.map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -7,16 +7,15 @@ package pers.chh3213.Set;
  * @version : 1.0
  * @Project : Collection_Map
  * @Package : pers.chh3213.Set
- * @FileName : TreeSetTest.java
+ * @FileName : TreeMapTest.java
  * @createTime : 2022/1/11 下午10:36
  * @Email :
  * @Description :
  */
 import org.junit.Test;
+import sun.reflect.generics.tree.Tree;
 
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * 1.向TreeSet中添加的数据，要求是相同类的对象。
@@ -24,40 +23,31 @@ import java.util.TreeSet;
  * 3.自然排序中，比较两个对象是否相同的标准为：compareTo()返回0.不再是equals().
  * 4.定制排序中，比较两个对象是否相同的标准为：compare()返回0.不再是equals().
  */
-public class TreeSetTest {
+public class TreeMapTest {
 
     @Test
     public void test() {
-        TreeSet set = new TreeSet();
+        TreeMap map = new TreeMap();
 
-        //失败：不能添加不同类的对象
-//        set.add(123);
-//        set.add(456);
-//        set.add("AA");
-//        set.add(new User("Tom",12));
+        // 自然排序
+        map.put(new User("Tom",12),10);
+        map.put(new User("Jerry",32),11);
+        map.put(new User("Jim",2),12);
+        map.put(new User("Mike",65),14);
+        map.put(new User("Jack",33),8);
+        map.put(new User("Jack",56),5);
 
-        //举例一：
-//        set.add(34);
-//        set.add(-34);
-//        set.add(43);
-//        set.add(11);
-//        set.add(8);
-
-        //举例二：
-        set.add(new User("Tom",12));
-        set.add(new User("Jerry",32));
-        set.add(new User("Jim",2));
-        set.add(new User("Mike",65));
-        set.add(new User("Jack",33));
-        set.add(new User("Jack",56));
-
-        Iterator iterator = set.iterator();
+        Set entry = map.entrySet();
+        Iterator iterator = entry.iterator();
         while(iterator.hasNext()){
-            System.out.println(iterator.next());
+            Object o = iterator.next();
+            Map.Entry entry1 = (Map.Entry) o;
+            System.out.println(((Map.Entry<?, ?>) o).getKey()+"-->"+((Map.Entry<?, ?>) o).getValue());
         }
     }
     @Test
     public void tets2(){
+        //定制排序
         Comparator com = new Comparator() {
             //按照年龄从小到大排列
             @Override
@@ -72,19 +62,21 @@ public class TreeSetTest {
             }
         };
 
-        TreeSet set = new TreeSet(com);
-        set.add(new User("Tom",12));
-        set.add(new User("Jerry",32));
-        set.add(new User("Jim",2));
-        set.add(new User("Mike",65));
-        set.add(new User("Mary",33));
-        set.add(new User("Jack",33));
-        set.add(new User("Jack",56));
+        TreeMap map = new TreeMap(com);
+        map.put(new User("Tom",12),10);
+        map.put(new User("Jerry",32),11);
+        map.put(new User("Jim",2),12);
+        map.put(new User("Mike",65),14);
+        map.put(new User("Jack",33),8);
+        map.put(new User("Jack",56),5);
 
 
-        Iterator iterator = set.iterator();
+        Set entry = map.entrySet();
+        Iterator iterator = entry.iterator();
         while(iterator.hasNext()){
-            System.out.println(iterator.next());
+            Object o = iterator.next();
+            Map.Entry entry1 = (Map.Entry) o;
+            System.out.println(((Map.Entry<?, ?>) o).getKey()+"-->"+((Map.Entry<?, ?>) o).getValue());
         }
     }
 }
