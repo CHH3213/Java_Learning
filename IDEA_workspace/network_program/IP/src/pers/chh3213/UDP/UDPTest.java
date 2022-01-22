a905d9b6582a7bf3bb2d8a2d3bf3cacf9d67c9d5
CHH3213 <1213472983@qq.com>
2022-01-22 16:26:03 +0800

    Squashed commit of the following:
    commit b806017d00797c26b96fdb89a3fa919eed401f3a
    Author: CHH3213 <1213472983@qq.com>
    Date:   Sat Jan 22 16:11:45 2022 +0800
    
        网络编程下
    
    commit 448813bbb5ac5dd524b344fb69cecbc27e9841f7
    Author: CHH3213 <1213472983@qq.com>
    Date:   Sat Jan 22 16:11:36 2022 +0800
    
        网络编程上
    
    commit 8e8a07ca43080a8716e8c39f98ae91e1771f3fbb
    Author: CHH3213 <1213472983@qq.com>
    Date:   Fri Jan 21 17:14:47 2022 +0800
    
        Squashed commit of the following:
    
        commit 644786b5a926cdec4727f8357c5343be401c90ce
        Author: CHH3213 <1213472983@qq.com>
        Date:   Fri Jan 21 17:11:33 2022 +0800
    
            IO流2
    
        commit cae57c3e4c612eea30c21df056a945516778289d
        Author: CHH3213 <1213472983@qq.com>
        Date:   Fri Jan 21 17:11:26 2022 +0800
    
            IO流1
    
        commit 640494e7ea5ae8bfb352091c8486482848e10854
        Author: CHH3213 <1213472983@qq.com>
        Date:   Mon Jan 17 11:08:26 2022 +0800
    
            IO流
    
    commit 2fd28ce6670181e88300b1e6d2a3b3db809ef592
    Author: CHH3213 <1213472983@qq.com>
    Date:   Mon Jan 17 20:17:56 2022 +0800
    
        Squashed commit of the following:
    
        commit 640494e7ea5ae8bfb352091c8486482848e10854
        Author: CHH3213 <1213472983@qq.com>
        Date:   Mon Jan 17 11:08:26 2022 +0800
    
            IO流

---
+++ IDEA_workspace/network_program/IP/src/pers/chh3213/UDP/UDPTest.java
@@ -0,0 +1,55 @@
+package pers.chh3213.UDP;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : chh3213
+ * @version : 1.0
+ * @Project : network_program
+ * @Package : pers.chh3213.UDP
+ * @ClassName : UDPTest.java
+ * @createTime : 2022/1/22 13:05
+ * @Email :
+ * @Description :
+ */
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ * UDPd协议的网络编程
+ */
+public class UDPTest {
+
+    //发送端
+    @Test
+    public void sender() throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+
+        String str = "我是UDP发送端";
+        byte[] data = str.getBytes();
+        InetAddress inet = InetAddress.getLocalHost();
+        DatagramPacket packet = new DatagramPacket(data,0,data.length,inet,9090);
+
+        socket.send(packet);
+        socket.close();
+    }
+
+    //接收端
+    @Test
+    public void receiver() throws IOException {
+        DatagramSocket socket = new DatagramSocket(9090);
+
+        byte[] buffer = new byte[100];
+        DatagramPacket packet = new DatagramPacket(buffer,0,buffer.length);
+
+        socket.receive(packet);
+
+        System.out.println(new String(packet.getData(),0,packet.getLength()));
+
+        socket.close();
+    }
+}
